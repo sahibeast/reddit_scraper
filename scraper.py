6bd3251609bba742d8c2ab963caaa230a7d4011e
Sahil Shah <deltaspin@gmail.com>
2013-05-15 01:44:19 -0700

    added comments

--- scraper.py
+++ scraper.py
@@ -4,6 +4,8 @@
 import requests
 from bs4 import BeautifulSoup, NavigableString
 
+#get specified item
+#in this case, vilalgeporn url for the front page of the subreddit
 def get_item(r):
     data = r.json()
 
@@ -12,10 +14,13 @@ def get_item(r):
     for i, v in enumerate(data["data"]["children"]):
         print v["data"]["url"]
 
+#gets the request object from passed in url
 def get_request (url):
     r = requests.get(url)
     get_item(r)
 
+#gets url from user and passes to get_request funtion
+#normally would have input of some sort but in this case i defaulted to the below link
 def get_url():
     url = 'http://reddit.com/r/villageporn.json'
     
